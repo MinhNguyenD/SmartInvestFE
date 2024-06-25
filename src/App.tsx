fd1511f93fe992037bae10d00fb380b15b4f1ebd
Ben Nguyen <nguyenducminh1307@gmail.com>
2024-06-25 16:47:16 -0300

    Scaffolding project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center">
+        <Button>Button</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
